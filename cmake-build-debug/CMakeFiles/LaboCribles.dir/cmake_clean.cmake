1e88faad061938a6440dc99abb151f23e24d7ab6
Théo Coutaudier <theo.coutaudier@heig-vd.ch>
2021-11-19 09:30:06 +0100

    add main

---
+++ cmake-build-debug/CMakeFiles/LaboCribles.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LaboCribles.dir/main.cpp.obj"
+  "LaboCribles.exe"
+  "LaboCribles.exe.manifest"
+  "LaboCribles.pdb"
+  "libLaboCribles.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LaboCribles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
