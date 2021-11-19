73cbe5fded4b0a84fd71833b4b255f6ce08d42d9
Grégoire Guyot <gguyot1023@gmail.com>
2021-11-19 14:27:05 +0100

    bite

--- affichageTableau.cpp
+++ affichageTableau.cpp
@@ -1,37 +1,31 @@
 /*
 -----------------------------------------------------------------------------------
-Nom du fichier : main.cpp
+Nom du fichier : affichageTableau.cpp
 Nom du Labo    : Labo 6 - Cribles
 
 Auteur(s)      : Grégoire Guyot et Théo Coutaudier
 
-Date creation  : 19-11-2021
+Date creation  : 10-11-2021
 
 Description    :
 
-Remarque(s)    : n/a
+Remarque(s)    :
 
 Compilateur    : Mingw-w64 g++ 11.2.0
 -----------------------------------------------------------------------------------
 */
 
-#include <cstdlib>
-#include <iostream>
-#include <limits>
 #include "affichageTableau.h"
 
 using namespace std;
 
-int main() {
-
-	bool tab[3] = {1, 0, 1};
-
-	afficherCribles(tab, 3, 2, 3);
-
-
-	cout << "Merci d'avoir joue. Appuyez sur ENTER pour terminer le programme...";
-	cin.ignore(numeric_limits<streamsize>::max(), '\n');
-	return EXIT_SUCCESS;
+void afficherCribles(const bool tab[], size_t taille, int nbColonnes, int
+espacement){
+	for (int j = 0; j <= nbColonnes; ++j){
+		for (size_t i = 0; i < taille; ++i){
+			cout << setw(espacement) << (tab[i] ? 'o' : 'x');
+		}
+		cout << endl;
+	}
 }
-
 
---
+++ affichageTableau.h
@@ -0,0 +1,30 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier : affichageTableau.h
+Nom du Labo    : Labo 6 - Cribles
+
+Auteur(s)      : Grégoire Guyot et Théo Coutaudier
+
+Date creation  : 10-11-2021
+
+Description    :
+
+Remarque(s)    :
+
+Compilateur    : Mingw-w64 g++ 11.2.0
+-----------------------------------------------------------------------------------
+*/
+
+#ifndef LABOCRIBLES_AFFICHAGETABLEAU_H
+#define LABOCRIBLES_AFFICHAGETABLEAU_H
+
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+
+void afficherCribles(const bool tab[],
+							size_t taille,
+							int nbColonnes,
+							int espacement);
+
+#endif //LABOCRIBLES_AFFICHAGETABLEAU_H
