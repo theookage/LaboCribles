dd59b35a59d3cf0b92429d3d8a474d1f412c60de
Théo Coutaudier <theo.coutaudier@heig-vd.ch>
2021-11-21 23:18:50 +0100

    ajout commentaire

--- CountListTableau.cpp
+++ CountListTableau.cpp
@@ -1,14 +1,21 @@
 /*
-------------------------------------------------------
-Nom du fichier : <nom du fichier>.<xxx>
-Auteur(s) : Theo Coutaudier
-Date creation : <jj.mm.aaaa>
-Nom Labo :
-Description :
+-----------------------------------------------------------------------------------
+Nom du fichier :  main.cpp
+Nom du Labo    :  Labo 6 - Cribles
 
-Remarque(s) :
-Compilateur : Mingw-w64 g++ 11.2.0
-------------------------------------------------------
+Auteur(s)      :  Grégoire Guyot et Théo Coutaudier
+
+Date creation  :  19-11-2021
+
+Description    :  La fonction countTrueElement compte le nombre de valeur à true
+                  dans le tableau.
+                  La fonction listTrueElement liste les cases du tableau qui sont
+                  à true.
+
+Remarque(s)    :
+
+Compilateur    :  Mingw-w64 g++ 11.2.0
+-----------------------------------------------------------------------------------
 */
 
 #include <limits>
@@ -19,7 +26,7 @@ Compilateur : Mingw-w64 g++ 11.2.0
 using namespace std;
 
 size_t countTrueElement(const bool tab[], size_t taille){
-   size_t count = 0;
+   size_t count = 0; //intitialisation du counter qui sera retourner
    for (size_t i = 0; i < taille; ++i) {
       if (tab[i]) {
          count++;
@@ -28,11 +35,12 @@ size_t countTrueElement(const bool tab[], size_t taille){
    return count;
 }
 void listTrueElement(const bool tab[], size_t taille, int nbParLigne, int espace) {
-   int count=0;
+   int count = 0;
    for (size_t i = 1; i < taille; ++i) {
       if(tab[i]) {
             cout << setw(espace) << i + 1;
             count++;
+            //vérifie si le programme doit passé à la ligne
             if (!(count%nbParLigne) && count!=0){
                cout<<endl;
             }
--- main.cpp
+++ main.cpp
@@ -1,15 +1,22 @@
 /*
 -----------------------------------------------------------------------------------
-Nom du fichier : main.cpp
-Nom du Labo    : Labo 6 - Cribles
+Nom du fichier :  main.cpp
+Nom du Labo    :  Labo 6 - Cribles
 
-Auteur(s)      : Grégoire Guyot et Théo Coutaudier
+Auteur(s)      :  Grégoire Guyot et Théo Coutaudier
 
-Date creation  : 19-11-2021
+Date creation  :  19-11-2021
 
-Description    :
+Description    :  L'utilisateur choisi une valeur entre 2 et 100, le programme va
+                  créer un tableau avec le nombre de valeur choisis par l'utilisateur,
+                  un algorithme va chercher les nombres premier entre 0 et la
+                  valeur saisie.
 
-Remarque(s)    : n/a
+Remarque(s)    :  La saisie de l'utilisateur est controlée
+                  Le constante peuvent être modifier pour afficher les tableau sur
+                  plus/moins de colonne ou plus d'espace entre les valeurs
+                  Lors de la création du tableau, il y a un avertissement, car le
+                  tableau pourrait être crée, mais cela n'arrive pas.
 
 Compilateur    : Mingw-w64 g++ 11.2.0
 -----------------------------------------------------------------------------------
@@ -32,11 +39,11 @@ int main() {
    // Décalaration constante
    //--------------------------------------
 
-   const unsigned int   MIN         = 2;        //testSaisie
-   const unsigned int   MAX         = 100;      //testSaisie
+   const unsigned int   MIN         = 2;        //valeur minimum testSaisie
+   const unsigned int   MAX         = 100;      //valeur maximum testSaisie
    const bool           valeur      = true;     //initTableau
-   const int            space       = 3;        //setw()
-   const int            nbcolonne  = 10;       //définir taille affichage tableau
+   const int            space       = 3;        //espace pour le setw()
+   const int            nbcolonne  = 10;        //définir taille affichage tableau
 
 
    //--------------------------------------
@@ -46,21 +53,22 @@ int main() {
    cout << "Nombre de valeur [" << MIN << " - " << MAX << "] : ";
    size_t taille = size_t(testSaisie(MIN, MAX));
 	bool tab[taille];
+
    //--------------------------------------
    // init tableau et affichage
    //--------------------------------------
 
    cout << endl << "initialisation du tableau... " << endl;
-   initTableau(tab, taille, valeur);
-   afficherCribles(tab, taille, nbcolonne, space);
+   initTableau(tab, taille, valeur); //initialisation du tableau
+   afficherCribles(tab, taille, nbcolonne, space); //affichage du tableau
 
    //--------------------------------------
    // modif tableau et affichage
    //--------------------------------------
-   
+
    cout << endl << "modification du tableau... " << endl;
-	criblage(tab, taille);
-	afficherCribles(tab, taille, nbcolonne, space);
+	criblage(tab, taille); //modification du tableau
+	afficherCribles(tab, taille, nbcolonne, space); //affichage du tableau
 
    //--------------------------------------
    // affichage liste nombre premier
@@ -68,7 +76,7 @@ int main() {
 
    cout << endl << "Il y a " << countTrueElement(tab, taille)
         << " nombre premier" << endl;
-   listTrueElement(tab, taille, nbcolonne , space);
+   listTrueElement(tab, taille, nbcolonne , space); //liste de tout les nb premier
 
    //--------------------------------------
    // Fin de programme
